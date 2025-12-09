57310295572c54f76aa4d60981b488998443ee6c
6704062610127 นายนนทวัฒน์ ร่มสายหยุด <s6704062610127@email.kmutnb.ac.th>
2025-12-09 14:37:16 +0700

    sale.c
    calculate promotion

---
+++ sale.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    float p1, p2, p3;
+
+    scanf("%f", &p1);
+    printf("%.2f\n", p1 * 0.95);
+
+    scanf("%f", &p2);
+    printf("%.2f\n", (p1 + p2) * 0.85);
+
+    scanf("%f", &p3);
+    printf("%.2f\n", (p1 + p2 + p3) * 0.70);
+
+    return 0;
+}
