442f841c784cfb79dc7003d96fe7a4f94e5e4395
6704062610127 นายนนทวัฒน์ ร่มสายหยุด <s6704062610127@email.kmutnb.ac.th>
2025-12-09 14:39:12 +0700

    sale.c
    calculate VAT

--- sale.c
+++ sale.c
@@ -4,13 +4,13 @@ int main() {
     float p1, p2, p3;
 
     scanf("%f", &p1);
-    printf("%.2f\n", p1 * 0.95);
+    printf("%.2f\n", (p1 * 0.95) * 1.07);
 
     scanf("%f", &p2);
-    printf("%.2f\n", (p1 + p2) * 0.85);
+    printf("%.2f\n", ((p1 + p2) * 0.85) * 1.07);
 
     scanf("%f", &p3);
-    printf("%.2f\n", (p1 + p2 + p3) * 0.70);
+    printf("%.2f\n", ((p1 + p2 + p3) * 0.70) * 1.07);
 
     return 0;
 }
